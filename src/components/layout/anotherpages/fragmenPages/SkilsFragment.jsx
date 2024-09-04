480af15bf85219383f37fed31b534dcad3db7dd5
musyafaid <machmadsaputra@gmail.com>
2024-09-04 13:30:43 +0700

    Fixed skills section

--- src/components/layout/anotherpages/fragmenPages/SkilsFragment.jsx
+++ src/components/layout/anotherpages/fragmenPages/SkilsFragment.jsx
@@ -1,29 +1,52 @@
-import React from 'react'
+import React from 'react';
 
 export default function SkilsFragment() {
+  const imgLink1 = [
+    { link: "/pngwing2.png" },
+    { link: "/react.svg" },
+    { link: "/pngegg.png" },
+    { link: "/Tailwind.png" },
+    { link: "/pngegg2.png" },
+  ];
+
+  const imgLink2 = [
+    { link: "/pngwing.png", alt: "PNG Wing" },
+    { link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", alt: "JavaScript" },
+    { link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg", alt: "PHP" },
+    { link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", alt: "HTML5" },
+    { link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", alt: "CSS3" },
+  ];
+
   return (
     <div className='w-44 my-8'>
       <h3 className='text-base font-semibold'>Current favorite stack</h3>
 
-
-      <div className=' border-none flex flex-col justify-start  w-auto  border rounded-sm'>
+      <div className='border-none flex flex-col justify-start w-auto border rounded-sm'>
         <div className='flex py-1 gap-2 justify-evenly'>
-        <img className='size-10 grayscale hover:grayscale-0' src="/pngwing2.png" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="/react.svg" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="/pngegg.png" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="/Tailwind.png" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="/pngegg2.png" alt="" />
-          
+          {imgLink1.map((img, index) => (
+            <img
+              key={index}
+              className='w-10 h-10 grayscale hover:grayscale-0 cursor-pointer'
+              src={img.link}
+              alt={`Image ${index + 1}`}
+            />
+          ))}
         </div>
-        <div className='flex py-1 gap-2 justify-evenly'>
-          
-        <img className='size-10 grayscale hover:grayscale-0' src="/pngwing.png" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="" />
-        <img className='size-10 grayscale hover:grayscale-0' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="" />
+
+        {/* Second set of images */}
+        <div className='flex py-1 gap-2 justify-evenly mt-4'>
+          {imgLink2.map((img, index) => (
+            <img
+              key={index}
+              className='w-10 h-10 grayscale hover:grayscale-0 cursor-pointer'
+              src={img.link}
+              alt={img.alt}
+            />
+          ))}
         </div>
       </div>
+
+     
     </div>
-  )
+  );
 }
